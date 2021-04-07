c9a440abca97295b73b3463ecef00f4b3f489c8f
frynameDev <psimen1986@gmail.com>
2021-04-07 18:15:04 +0300

    added main screen

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="previous slide">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="next slide">',
+    asNavFor: '.slider-dots',
+  });
 
-})
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+  });
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ function images() {
 
 function scripts() {
    return src([
-      'node_modules/jquery/dist/jquery.js',
+      // 'node_modules/jquery/dist/jquery.js',
       'app/js/main.js'
    ])
       .pipe(concat('main.min.js'))
@@ -49,7 +49,9 @@ function scripts() {
 }
 
 function styles() {
-   return src('app/scss/style.scss')
+   return src([
+      'node_modules/animate.css/animate.css',
+      'app/scss/style.scss'])
       .pipe(scss({ outputStyle: 'compressed' }))
       .pipe(concat('style.min.css'))
       .pipe(autoprefixer({
@@ -59,6 +61,17 @@ function styles() {
       .pipe(dest('app/css'))
       .pipe(browserSync.stream())
 }
+// function styles() {
+//    return src('app/scss/style.scss')
+//       .pipe(scss({ outputStyle: 'compressed' }))
+//       .pipe(concat('style.min.css'))
+//       .pipe(autoprefixer({
+//          overrideBrowserslist: ['last 10 version'],
+//          grid: true
+//       }))
+//       .pipe(dest('app/css'))
+//       .pipe(browserSync.stream())
+// }
 
 function build() {
    return src([
