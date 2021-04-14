af2402e449c225f49ad80b9fe1927540af9f42cc
frynameDev <psimen1986@gmail.com>
2021-04-15 01:06:31 +0300

    added shop page and footer

--- app/js/main.js
+++ app/js/main.js
@@ -30,11 +30,59 @@ $(function () {
     focusOnSelect: true,
   });
 
-  $('.travel__slider').slick({
+  $('.holder__slider, .shop__slider').slick({
     infinite: true,
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="previous slide">',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="next slide">',
+  });
+
+
+
+  $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/plus.svg" alt="button: plus"></div><div class="quantity-button quantity-down"><img src="images/minus.svg" alt="button: minus"></div></div>').insertAfter('.quantity input');
+  $('.quantity').each(function () {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
+
+    btnUp.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+  });
+
+  $('.quantity-button').on('click', function () {
+    let summ = $('.nights').val() * $('.summ').data('nights') + ($('.quests').val() - 1) * $('.summ').data('quests');
+    $('.summ').html('$' + summ);
+  });
+
+  let summ = $('.nights').val() * $('.summ').data('nights') + ($('.quests').val() - 1) * $('.summ').data('quests');
+  $('.summ').html('$' + summ);
+
+
+  $('.surfboard-box__circle').on('click', function () {
+    $(this).toggleClass('active')
   });
 
 });
