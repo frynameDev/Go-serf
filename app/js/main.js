6f95324b507fff31debe350f114f67935831fcc8
frynameDev <psimen1986@gmail.com>
2021-04-17 00:53:39 +0300

    added animate

--- app/js/main.js
+++ app/js/main.js
@@ -138,4 +138,7 @@ $(function () {
     $('.menu').toggleClass('active');
   });
 
+
+  new WOW().init();
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ function images() {
 function scripts() {
    return src([
       // 'node_modules/jquery/dist/jquery.js',
+      'node_modules/wow.js/dist/wow.js',
       'app/js/main.js'
    ])
       .pipe(concat('main.min.js'))
